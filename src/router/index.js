fbfeb18f906a39c1d4527ef5bfe0733bc42ad182
Nurkenproga <n_atabay@kbru.com>
2025-10-19 15:54:55 +0500

    Article Page without api

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import ArticlePage from '../pages/ArticlePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: () => import('../pages/HomePage.vue')
+    // },
+    {
+      path: '/',
+      name: 'article',
+      component: ArticlePage,
+      props: true
+    },
+    // {
+    //   path: '/favorites',
+    //   name: 'favorites',
+    //   component: () => import('../pages/FavoritesPage.vue')
+    // }
+  ],
 })
 
 export default router
